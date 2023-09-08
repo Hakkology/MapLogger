d23338787b0100369735c2e4eb3837ed5b8537f2
Hakkology <yildizhakan88@gmail.com>
2023-09-08 15:20:34 +0300

    Index.html included with both maps.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,14 +7,18 @@ namespace MapLogger.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public IActionResult Index()
     {
+        var cesiumToken = _configuration["Cesium:AccessToken"];
+        ViewData["CesiumToken"] = cesiumToken;
         return View();
     }
 
