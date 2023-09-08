268365adff94d9412c7419bfc09be6f01ba3206a
Hakkology <yildizhakan88@gmail.com>
2023-09-08 15:13:51 +0300

    Index.html included with both maps.

---
+++ notes.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapLogger
+{
+    public class Notes
+    {
+
+/*  Openlayers haritası ve Cesium 3B harita görüntüleyicisini birlikte gösteren bir proje yapmanızı istiyoruz, tek bir html sayfası. 
+    Openlayers haritası ekranın solunda olacak ve Cesium görüntüleyicisi de sağda. Boydan %50-50 böl ekranı. 
+    Yapıldı
+
+    Açılışta soldaki haritanın gösterdiği alan aynı zamanda Cesium görüntüleyicisinde de görünecek. 
+    Her iki harita da başta açık olacak yani.
+
+    Cesium haritasında arazi modelinin de görüntülenmesini istiyoruz. 
+    Bunun için yapman gereken iş basit, sadece bilmiyorsan bir küçük araştırma yapman lazım.
+
+    Haritalar açılığında Ankara Kızılay’ı merkez alacak şekilde bir zoom seviyesinde (ve 3D harita için gerekli görüş açısında) gelsinler lütfen. 
+    Bundan sonra, soldaki 2D haritaya her tıklayışta sağdaki Cesium haritası da tıklanılan yerin koordinatlarına animasyonla gitsin ve 
+    bu coğrafi koordinatlar konsola loglansın. Bu sırada Cesium haritası hiç bir zaman yer altına inmemeli ve 
+    soldaki haritada tıklanılan yer, sağdaki haritada viewpoint’in dışında kalmamalı.
+
+    Sağdaki (Cesium) haritasında herhangi bir yere tıkladığınızda ise bu sefer soldaki haritanın merkezi o noktaya kaydırılsın ve 
+    bu coğrafi koordinatlar konsola loglansın.
+
+    Her iki harita tıklamaları da bir yandan sunucu tarafında bir uygulamaya gönderilsin ve tek bir text dosyasına loglansın. 
+    Bunu nasıl yapacağın önemli değil, sadece bizim nasıl çalıştıracağımızı tarif etmeni bekleriz. 
+    Bu uygulamanın çok kullanıcılı olduğunu ve aynı anda çok fazla loglama isteğinin sunucuya geleceğini ve 
+    her birinin text dosyaya loglanması gerektiğini varsayalım.
+*/
+
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,56 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function initMaps(cesiumAccessToken){
+
+    Cesium.Ion.defaultAccessToken = cesiumAccessToken;
+
+    var openlayersView = new ol.View({
+        center: ol.proj.fromLonLat([32.8597, 39.9208]),
+        zoom: 14
+    });
+
+    var olMap = new ol.Map({
+        target: 'map-container',
+        layers: [new ol.layer.Tile({source: new ol.source.OSM()})],
+        view: openlayersView
+    });
+
+    var cesiumViewer = new Cesium.Viewer('cesiumContainer', {
+        imageryProvider: new Cesium.IonImageryProvider({ assetId: 3812 }),
+        terrainProvider: Cesium.createWorldTerrain(),
+        animation: false,
+        baseLayerPicker: false,
+        geocoder: false,
+        homeButton: false,
+        sceneModePicker: false,
+        timeline: false,
+        navigationHelpButton: false,
+        vrButton: false,
+        initialViewRectangle: Cesium.Rectangle.fromDegrees(32.8597 - 0.1, 39.9208 - 0.1, 32.8597 + 0.1, 39.9208 + 0.1)
+    });
+
+    olMap.on('click', function(evt) {
+        var coords = ol.proj.toLonLat(evt.coordinate);
+        cesiumViewer.camera.flyTo({destination: Cesium.Cartesian3.fromDegrees(coords[0], coords[1], 1500)});
+        console.log(coords);
+        // Burada sunucuya istek atıp koordinatları loglayabilirsiniz
+    });
+
+    var handler = new Cesium.ScreenSpaceEventHandler(cesiumViewer.scene.canvas);
+    handler.setInputAction(function(movement) {
+        var pickedFeature = cesiumViewer.scene.pick(movement.position);
+        if (pickedFeature) {
+            var cartesian = cesiumViewer.scene.pickPosition(movement.position);
+            var cartographic = Cesium.Cartographic.fromCartesian(cartesian);
+            var longitude = Cesium.Math.toDegrees(cartographic.longitude);
+            var latitude = Cesium.Math.toDegrees(cartographic.latitude);
+            
+            openlayersView.animate({center: ol.proj.fromLonLat([longitude, latitude])});
+            console.log([longitude, latitude]);
+            // Rest of your code...
+        }
+    }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+    
+}
